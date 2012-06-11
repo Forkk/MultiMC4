76da114143c047b559759a0344c60a7d9aac372d
Andrew Okin <razorblades13@gmail.com>
2012-06-11 11:43:44 -0500

    Added about dialog.

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -32,6 +32,7 @@
 #include <wx/dir.h>
 #include <wx/fs_arc.h>
 #include <wx/fs_mem.h>
+#include <wx/aboutdlg.h>
 
 IMPLEMENT_APP(MultiMC)
 
@@ -254,7 +255,29 @@ void MainWindow::OnHelpClicked(wxCommandEvent& event)
 
 void MainWindow::OnAboutClicked(wxCommandEvent& event)
 {
-	NotImplemented();
+	wxAboutDialogInfo info;
+	info.SetName(_("MultiMC"));
+	info.SetVersion(AppVersion.ToString() + _(" Beta"));
+	info.SetDescription(_("MultiMC is a custom launcher that makes managing Minecraft easier by allowing you to have multiple installations of Minecraft at once."));
+	info.SetCopyright(_("(C) 2012 Andrew Okin"));
+	info.SetWebSite(_("http://forkk.net/MultiMC"));
+	info.SetLicense(_("Licensed under the Apache License, Version 2.0 (the \"License\");\n\
+you may not use this file except in compliance with the License.\n\
+You may obtain a copy of the License at\n\
+\n\
+\thttp://www.apache.org/licenses/LICENSE-2.0\n\
+\n\
+Unless required by applicable law or agreed to in writing, software\n\
+distributed under the License is distributed on an \"AS IS\" BASIS,\n\
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n\
+See the License for the specific language governing permissions and\n\
+limitations under the License."));
+	info.SetIcon(wxGetApp().GetAppIcon());
+	info.AddDeveloper(_("Andrew Okin <forkk@forkk.net>"));
+	info.AddDeveloper(_("Petr Mrázek"));
+	
+	
+	wxAboutBox(info);
 }
 
 
