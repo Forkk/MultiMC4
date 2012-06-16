818b407759feb1e0bda52a0b7ba71ae3aade51b7
Andrew Okin <razorblades13@gmail.com>
2012-06-16 02:43:44 -0500

    Revert "Fixed Linux toolchain file."
    This reverts commit 6677882a7735637fbbcb938ed58d3b6eab3eeb30.

--- Linux.cmake
+++ Linux.cmake
@@ -1,3 +1,14 @@
 # which compilers to use for C and C++
 SET(CMAKE_C_COMPILER gcc)
 SET(CMAKE_CXX_COMPILER g++)
+
+# here is the target environment located
+SET(CMAKE_FIND_ROOT_PATH /usr)
+SET(wxWidgets_ROOT_DIR /usr)
+
+# adjust the default behaviour of the FIND_XXX() commands:
+# search headers and libraries in the target environment, search 
+# programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
