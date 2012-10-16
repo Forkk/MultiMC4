5b722ae23e699f8a75090c554355945f6dfe7a31
Petr Mrázek <peterix@gmail.com>
2012-10-16 11:44:16 +0200

    Parse java class annotations

--- src/java/classfile.h
+++ src/java/classfile.h
@@ -1,226 +1,20 @@
 #pragma once
-#include <stdint.h>
-#include <string>
-#include <vector>
-#include <exception>
-#include "endian.h"
-
+#include "membuffer.h"
+#include "constants.h"
+#include "annotations.h"
+#include <map>
 namespace java
 {
-	class membuffer
-	{
-	public:
-		membuffer(char * buffer, std::size_t size)
-		{
-			current = start = buffer;
-			end = start + size;
-		}
-		~membuffer()
-		{
-			// maybe? possibly? left out to avoid confusion. for now.
-			//delete start;
-		}
-		/**
-		 * Read some value. That's all ;)
-		 */
-		template <class T>
-		void read(T& val)
-		{
-			val = *(T *)current;
-			current += sizeof(T);
-		}
-		/**
-		 * Read a big-endian number
-		 * valid for 2-byte, 4-byte and 8-byte variables
-		 */
-		template <class T>
-		void read_be(T& val)
-		{
-			val = bigswap(*(T *)current);
-			current += sizeof(T);
-		}
-		/**
-		 * Read a string in the format:
-		 * 2B length (big endian, unsigned)
-		 * length bytes data
-		 */
-		void read_jstr(std::string & str)
-		{
-			uint16_t length = 0;
-			read_be(length);
-			str.append(current,length);
-			current += length;
-		}
-		/**
-		 * Skip N bytes
-		 */
-		void skip (std::size_t N)
-		{
-			current += N;
-		}
-	private:
-		char * start, *end, *current;
-	};
-	class classfile_exception : public std::exception {};
-	class constant
-	{
-	public:
-		enum type_t:uint8_t
-		{
-			j_string_data = 1,
-			j_int = 3,
-			j_float = 4,
-			j_long = 5,
-			j_double = 6,
-			j_class = 7,
-			j_string = 8,
-			j_fieldref = 9,
-			j_methodref = 10,
-			j_interface_methodref = 11,
-			j_nameandtype = 12
-		} type;
-		constant(membuffer & buf )
-		{
-			buf.read(type);
-			// invalid constant type!
-			if(type > j_nameandtype || type == (type_t)0 || type == (type_t)2)
-				throw new classfile_exception();
-			
-			// load data depending on type
-			switch(type)
-			{
-				case j_float:
-				case j_int:
-					buf.read_be(int_data); // same as float data really
-					break;
-				case j_double:
-				case j_long:
-					buf.read_be(long_data); // same as double
-					break;
-				case j_class:
-					buf.read_be(ref_type.class_idx);
-					break;
-				case j_fieldref:
-				case j_methodref:
-				case j_interface_methodref:
-					buf.read_be(ref_type.class_idx);
-					buf.read_be(ref_type.name_and_type_idx);
-					break;
-				case j_string:
-					buf.read_be(index);
-					break;
-				case j_string_data:
-					// HACK HACK: for now, we call these UTF-8 and do no further processing.
-					// Later, we should do some decoding. It's really modified UTF-8
-					// * U+0000 is represented as 0xC0,0x80 invalid character
-					// * any single zero byte ends the string
-					// * characters above U+10000 are encoded like in CESU-8
-					buf.read_jstr(str_data);
-					break;
-				case j_nameandtype:
-					buf.read_be(name_and_type.name_index);
-					buf.read_be(name_and_type.descriptor_index);
-					break;
-			}
-		}
-
-		std::string str_data; /** String data in 'modified utf-8'.*/
-		// store everything here.
-		union
-		{
-			int32_t int_data;
-			int64_t long_data;
-			float float_data;
-			double double_data;
-			uint16_t index;
-			struct
-			{
-				/**
-				 * Class reference:
-				 *   an index within the constant pool to a UTF-8 string containing
-				 *   the fully qualified class name (in internal format)
-				 * Used for j_class, j_fieldref, j_methodref and j_interface_methodref
-				 */
-				uint16_t class_idx;
-				// used for j_fieldref, j_methodref and j_interface_methodref
-				uint16_t name_and_type_idx;
-			} ref_type;
-			struct
-			{
-				uint16_t name_index;
-				uint16_t descriptor_index;
-			} name_and_type;
-		};
-	};
-	/**
-	 * A helper class that represents the custom container used in Java class file for storage of constants
-	 */
-	class constant_pool
-	{
-	public:
-		/**
-		 * Create a pool of constants
-		 */
-		constant_pool(){}
-		/**
-		 * Load a java constant pool
-		 */
-		void load(membuffer & buf)
-		{
-			uint16_t length = 0;
-			buf.read_be(length);
-			length --;
-			uint16_t index = 1;
-			const constant * last_constant = nullptr;
-			while(length)
-			{
-				const constant & cnst = constant(buf);
-				constants.push_back(cnst);
-				last_constant = &constants[constants.size() - 1];
-				if(last_constant->type == constant::j_double || last_constant->type == constant::j_long)
-				{
-					length-=2;
-					index+=2;
-				}
-				else
-				{
-					length--;
-					index++;
-				}
-			}
-		};
-		typedef std::vector<java::constant> container_type;
-		/**
-		 * Access constants based on jar file index numbers (index of the first element is 1)
-		 */
-		java::constant & operator[](std::size_t constant_index)
-		{
-			if(constant_index == 0 || constant_index > constants.size())
-			{
-				throw new classfile_exception();
-			}
-			return constants[constant_index - 1];
-		};
-		container_type::const_iterator begin() const
-		{
-			return constants.begin();
-		};
-		container_type::const_iterator end() const
-		{
-			return constants.end();
-		}
-	private:
-		container_type constants;
-	};
 	/**
 	 * Class representing a Java .class file
 	 */
-	class classfile : public membuffer
+	class classfile : public util::membuffer
 	{
 	public:
 		classfile(char * data, std::size_t size) : membuffer(data, size)
 		{
 			valid = false;
+			is_synthetic = false;
 			read_be(magic);
 			if(magic != 0xCAFEBABE)
 				throw new classfile_exception();
@@ -321,15 +115,29 @@ namespace java
 			read_be(class_attr_count);
 			while(class_attr_count)
 			{
-				skip(2);
+				uint16_t name_idx = 0;
+				read_be(name_idx);
 				uint32_t attr_length = 0;
 				read_be(attr_length);
-				skip(attr_length);
+				
+				auto name = constants[name_idx];
+				if(name.str_data == "RuntimeVisibleAnnotations")
+				{
+					uint16_t num_annotations = 0;
+					read_be(num_annotations);
+					while (num_annotations)
+					{
+						visible_class_annotations.push_back(annotation::read(*this, constants));
+						num_annotations --;
+					}
+				}
+				else skip(attr_length);
 				class_attr_count --;
 			}
 			valid = true;
 		};
 		bool valid;
+		bool is_synthetic;
 		uint32_t magic;
 		uint16_t minor_version;
 		uint16_t major_version;
@@ -340,5 +148,6 @@ namespace java
 		// interfaces this class implements ? must be. investigate.
 		std::vector<uint16_t> interfaces;
 		
+		java::annotation_table visible_class_annotations;
 	};
 }
--- src/java/endian.h
+++ src/java/endian.h
@@ -1,62 +1,62 @@
 #pragma once
 #include <stdint.h>
 
-namespace java
+/**
+ * Swap bytes between big endian and local number representation
+ */
+namespace util
 {
-	/**
-	 * Swap bytes between big endian and local number representation
-	 */
-	#ifdef MULTIMC_BIG_ENDIAN
-	uint64_t bigswap(uint64_t x)
-	{
-		return x;
-	};
-	uint32_t bigswap(uint32_t x)
-	{
-		return x;
-	};
-	uint16_t bigswap(uint16_t x)
-	{
-		return x;
-	};
-	int64_t bigswap(int64_t x)
-	{
-		return x;
-	};
-	int32_t bigswap(int32_t x)
-	{
-		return x;
-	};
-	int16_t bigswap(int16_t x)
-	{
-		return x;
-	};
-	#else
-	uint64_t bigswap(uint64_t x)
-	{
-		return (x>>56) | ((x<<40) & 0x00FF000000000000) | ((x<<24) & 0x0000FF0000000000) | ((x<<8)  & 0x000000FF00000000) |
-			   ((x>>8)  & 0x00000000FF000000) | ((x>>24) & 0x0000000000FF0000) | ((x>>40) & 0x000000000000FF00) | (x<<56);
-	};
-	uint32_t bigswap(uint32_t x)
-	{
-		return (x>>24) | ((x<<8) & 0x00FF0000) | ((x>>8) & 0x0000FF00) | (x<<24);
-	};
-	uint16_t bigswap(uint16_t x)
-	{
-		return (x>>8) | (x<<8);
-	};
-	int64_t bigswap(int64_t x)
-	{
-		return (x>>56) | ((x<<40) & 0x00FF000000000000) | ((x<<24) & 0x0000FF0000000000) | ((x<<8)  & 0x000000FF00000000) |
-			   ((x>>8)  & 0x00000000FF000000) | ((x>>24) & 0x0000000000FF0000) | ((x>>40) & 0x000000000000FF00) | (x<<56);
-	};
-	int32_t bigswap(int32_t x)
-	{
-		return (x>>24) | ((x<<8) & 0x00FF0000) | ((x>>8) & 0x0000FF00) | (x<<24);
-	};
-	int16_t bigswap(int16_t x)
-	{
-		return (x>>8) | (x<<8);
-	};
-	#endif
+#ifdef MULTIMC_BIG_ENDIAN
+inline uint64_t bigswap(uint64_t x)
+{
+	return x;
 };
+inline uint32_t bigswap(uint32_t x)
+{
+	return x;
+};
+inline uint16_t bigswap(uint16_t x)
+{
+	return x;
+};
+inline int64_t bigswap(int64_t x)
+{
+	return x;
+};
+inline int32_t bigswap(int32_t x)
+{
+	return x;
+};
+inline int16_t bigswap(int16_t x)
+{
+	return x;
+};
+#else
+inline uint64_t bigswap(uint64_t x)
+{
+	return (x>>56) | ((x<<40) & 0x00FF000000000000) | ((x<<24) & 0x0000FF0000000000) | ((x<<8)  & 0x000000FF00000000) |
+			((x>>8)  & 0x00000000FF000000) | ((x>>24) & 0x0000000000FF0000) | ((x>>40) & 0x000000000000FF00) | (x<<56);
+};
+inline uint32_t bigswap(uint32_t x)
+{
+	return (x>>24) | ((x<<8) & 0x00FF0000) | ((x>>8) & 0x0000FF00) | (x<<24);
+};
+inline uint16_t bigswap(uint16_t x)
+{
+	return (x>>8) | (x<<8);
+};
+inline int64_t bigswap(int64_t x)
+{
+	return (x>>56) | ((x<<40) & 0x00FF000000000000) | ((x<<24) & 0x0000FF0000000000) | ((x<<8)  & 0x000000FF00000000) |
+			((x>>8)  & 0x00000000FF000000) | ((x>>24) & 0x0000000000FF0000) | ((x>>40) & 0x000000000000FF00) | (x<<56);
+};
+inline int32_t bigswap(int32_t x)
+{
+	return (x>>24) | ((x<<8) & 0x00FF0000) | ((x>>8) & 0x0000FF00) | (x<<24);
+};
+inline int16_t bigswap(int16_t x)
+{
+	return (x>>8) | (x<<8);
+};
+#endif
+}
