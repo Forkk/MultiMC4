1832ca5c21d42f995a69fb4acbed48676ccda6f9
Petr Mrázek <peterix@gmail.com>
2012-11-21 00:38:16 +0100

    Fix MSVC build

--- src/utils/fsutils_secure.cpp
+++ src/utils/fsutils_secure.cpp
@@ -1,10 +1,8 @@
-namespace fsutils {
-
 #ifdef WIN32
 #define _WIN32_WINNT 0x0500
 #include <windows.h>
 #include <sddl.h>
-
+#include <wchar.h>
 // This constructs an ACL to give the current user file permissions.
 BOOL CreateMyDACL(SECURITY_ATTRIBUTES * pSA)
 {
@@ -12,7 +10,7 @@ BOOL CreateMyDACL(SECURITY_ATTRIBUTES * pSA)
 	HANDLE hToken = NULL;
 	if (!OpenProcessToken( GetCurrentProcess(), TOKEN_QUERY, &hToken ))
 	{
-		_tprintf( _T("OpenProcessToken failed. GetLastError returned: %d\n"), GetLastError());
+		//_tprintf( _T("OpenProcessToken failed. GetLastError returned: %d\n"), GetLastError());
 		return false;
 	}
 
@@ -20,7 +18,7 @@ BOOL CreateMyDACL(SECURITY_ATTRIBUTES * pSA)
 	DWORD dwBufferSize = 0;
 	if (!GetTokenInformation( hToken, TokenUser, NULL, 0, &dwBufferSize) && (GetLastError() != ERROR_INSUFFICIENT_BUFFER))
 	{
-		_tprintf( _T("GetTokenInformation failed. GetLastError returned: %d\n"), GetLastError());
+		//_tprintf( _T("GetTokenInformation failed. GetLastError returned: %d\n"), GetLastError());
 		// Cleanup
 		CloseHandle( hToken );
 		hToken = NULL;
@@ -33,7 +31,7 @@ BOOL CreateMyDACL(SECURITY_ATTRIBUTES * pSA)
 	// Retrieve the token information in a TOKEN_USER structure
 	if (!GetTokenInformation( hToken, TokenUser, pTokenUser, dwBufferSize, &dwBufferSize))
 	{
-		_tprintf( _T("2 GetTokenInformation failed. GetLastError returned: %d\n"), GetLastError());
+		//_tprintf( _T("2 GetTokenInformation failed. GetLastError returned: %d\n"), GetLastError());
 		// Cleanup
 		CloseHandle( hToken );
 		hToken = NULL;
@@ -44,7 +42,7 @@ BOOL CreateMyDACL(SECURITY_ATTRIBUTES * pSA)
 	// Check if SID is valid
 	if (!IsValidSid(pTokenUser->User.Sid))
 	{
-		_tprintf( _T("The owner SID is invalid.\n") );
+		//_tprintf( _T("The owner SID is invalid.\n") );
 		// Cleanup
 		CloseHandle(hToken);
 		hToken = NULL;
@@ -65,7 +63,7 @@ BOOL CreateMyDACL(SECURITY_ATTRIBUTES * pSA)
 	// 1024 bytes is enough for everyone. This is final.
 	wchar_t long_str[1024];
 	// give full access to the file to the user running this process and nobody else
-	swprintf(long_str,TEXT("D:P(A;OI;GA;;;%s)"),pszSID);
+	swprintf(long_str, 1024 ,TEXT("D:P(A;OI;GA;;;%s)"),pszSID);
 	if(pSA == NULL)
 	{
 		free(pTokenUser);
@@ -80,7 +78,7 @@ BOOL CreateMyDACL(SECURITY_ATTRIBUTES * pSA)
 	*/
 
 	// Convert the string to the security descriptor binary and return
-	bool result = ConvertStringSecurityDescriptorToSecurityDescriptor(
+	bool result = (bool) ConvertStringSecurityDescriptorToSecurityDescriptorW(
 		long_str,                         // The ACE strings
 		SDDL_REVISION_1,              // Standard revision level
 		&(pSA->lpSecurityDescriptor), // Pointer to the converted security descriptor
@@ -88,27 +86,31 @@ BOOL CreateMyDACL(SECURITY_ATTRIBUTES * pSA)
 	free(pTokenUser);
 	return result;
 }
-
-// only works on NTFS volumes.
-bool SecureFile (const char * filename)
+namespace fsutils
 {
-	SECURITY_ATTRIBUTES  sa;
-	sa.nLength = sizeof(SECURITY_ATTRIBUTES);
-	sa.bInheritHandle = FALSE;
-	if(!CreateMyDACL(&sa))
-		return false;
-	int result = SetFileSecurityA(filename,DACL_SECURITY_INFORMATION,sa.lpSecurityDescriptor);
-	LocalFree(sa.lpSecurityDescriptor);
-	return result != 0;
+	// only works on NTFS volumes.
+	bool SecureFile (const char * filename)
+	{
+		SECURITY_ATTRIBUTES  sa;
+		sa.nLength = sizeof(SECURITY_ATTRIBUTES);
+		sa.bInheritHandle = FALSE;
+		if(!CreateMyDACL(&sa))
+			return false;
+		int result = SetFileSecurityA(filename,DACL_SECURITY_INFORMATION,sa.lpSecurityDescriptor);
+		LocalFree(sa.lpSecurityDescriptor);
+		return result != 0;
+	}
 }
 #else
 #include <sys/stat.h>
-// Windows developers, gaze upon the perfection of this solution and weep. Weep blood.
-bool SecureFile (const char * filename)
+namespace fsutils
 {
-	int result = chmod(filename,S_IRUSR | S_IWUSR);
-	return result != -1;
+	// Windows developers, gaze upon the perfection of this solution and weep. Weep blood.
+	bool SecureFile (const char * filename)
+	{
+		int result = chmod(filename,S_IRUSR | S_IWUSR);
+		return result != -1;
+	}
 }
 #endif
 
-}
